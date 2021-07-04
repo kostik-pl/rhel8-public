cfe56b79f37870c2a22ca25ecf6829b24f016120
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2021-07-04 23:54:29 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Setup system
+dnf -y update
+dnf -y install mc
+dnf -y install pcp-system-tools
+systemctl enable pmcd
+sed 's/enforcing/disabled/g' /etc/selinux/config
+#Setup disk
+disk1="/dev/disk/by-label/_container /_container auto nosuid,nodev,nofail,x-gvfs-show 0 0"
+disk2="/dev/disk/by-label/_data /_data auto nosuid,nodev,nofail,x-gvfs-show 0 0"
+if ! grep -q '/_container' /etc/fstab ; then
+	printf "$disk1\n" >> /etc/fstab
+fi
+if ! grep -q '/_data' /etc/fstab ; then
+	printf "$disk2\n" >> /etc/fstab
+fi
+#Reboot
+reboot
