5ce5cf86b1d2c721cb3a92862360d9b76bd699a0
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2021-07-06 18:52:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,9 +20,10 @@ fi
 #Setup PODMAN
 dnf -y module install container-tools
 dnf -y install podman-docker
-
-#Create rootless user for pomdman
+sed -i 's/graphroot = "\/var\/lib\/containers\/storage"/graphroot = "\/_container"/g' /etc/containers/storage.conf
+#Create adn configure rootless user for pomdman
 sudo useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' 'RheujvDhfub72') podman
+mkdir -p /home/podman/.config/systemd/useradd
 
 #Reboot
 reboot
