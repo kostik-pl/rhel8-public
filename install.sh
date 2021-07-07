0c1caa947784b10f19d96276d839c60ecbe4a60b
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2021-07-07 22:00:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@ systemctl enable pmcd
 sed -i 's/enforcing/disabled/g' /etc/selinux/config
 
 #Setup disk
-FILE1=/dev/disk/by-label/_container
+FILE1="/dev/disk/by-label/_container"
 DISK1="/dev/disk/by-label/_container /_container auto nosuid,nodev,nofail,x-gvfs-show 0 0"
-FILE2=/dev/disk/by-label/_data
+FILE2="/dev/disk/by-label/_data"
 DISK2="/dev/disk/by-label/_data /_data auto nosuid,nodev,nofail,x-gvfs-show 0 0"
 if [ -L "$FILE1" ]
 then
     if ! grep -q '/_container' /etc/fstab ; then
-	printf "$DISK1\n" >> /etc/fstab
+	sed -i -e '"$DISK1\n"' /etc/fstab
     fi
 else
     echo "Disk labeled as $FILE1 not found"
@@ -24,7 +24,7 @@ fi
 if [ -L "$FILE2" ]
 then
     if ! grep -q '/_data' /etc/fstab ; then
-	printf "$DISK2\n" >> /etc/fstab
+	sed -i -e '"$DISK2\n" /etc/fstab
     fi
 else
     echo "Disk labeled as $FILE2 not found"
