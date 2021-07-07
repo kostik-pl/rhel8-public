0032edeebe94e28a3bde11fccc6a6c32be93ebf6
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2021-07-07 22:22:50 +0300

    updare

--- install.sh
+++ install.sh
@@ -31,13 +31,5 @@ else
     exit 1
 fi
 
-#Setup PODMAN
-dnf -y module install container-tools
-dnf -y install podman-docker
-sed -i 's/graphroot = "\/var\/lib\/containers\/storage"/graphroot = "\/_container"/g' /etc/containers/storage.conf
-#Create adn configure rootless user for pomdman
-#sudo useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' 'RheujvDhfub72') podman
-#mkdir -p /home/podman/.config/systemd/user
-
 #Reboot
 reboot
--- install1.sh
+++ install1.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+#Setup PODMAN
+dnf -y module install container-tools
+dnf -y install podman-docker
+sed -i 's/graphroot = "\/var\/lib\/containers\/storage"/graphroot = "\/_container"/g' /etc/containers/storage.conf
+#Create adn configure rootless user for pomdman
+#sudo useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' 'RheujvDhfub72') podman
+#mkdir -p /home/podman/.config/systemd/user
+
 podman run --name pgpro -d -v /_data:/_data docker.io/kostikpl/rhel8:pgpro-rhel8
