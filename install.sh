cd0e253eb74464653b50403d093d8a352a6064d4
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2021-07-07 20:30:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,12 +9,13 @@ sed -i 's/enforcing/disabled/g' /etc/selinux/config
 
 #Setup disk
 FILE1=/dev/disk/by-label/_container
+DISK1="/dev/disk/by-label/_container /_container auto nosuid,nodev,nofail,x-gvfs-show 0 0"
 FILE2=/dev/disk/by-label/_data
+DISK2="/dev/disk/by-label/_data /_data auto nosuid,nodev,nofail,x-gvfs-show 0 0"
 if [ -L "$FILE1" ]
 then
-    disk1="/dev/disk/by-label/_container /_container auto nosuid,nodev,nofail,x-gvfs-show 0 0"
     if ! grep -q '/_container' /etc/fstab ; then
-	printf "$disk1\n" >> /etc/fstab
+	printf "$DISK1\n" >> /etc/fstab
     fi
 else
     echo $FILE1 not found
@@ -22,9 +23,8 @@ else
 fi
 if [ -L "$FILE2" ]
 then
-    disk2="/dev/disk/by-label/_data /_data auto nosuid,nodev,nofail,x-gvfs-show 0 0"
     if ! grep -q '/_data' /etc/fstab ; then
-	printf "$disk2\n" >> /etc/fstab
+	printf "$DISK2\n" >> /etc/fstab
     fi
 else
     echo $FILE2 not found
