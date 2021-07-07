9e8e573ecf998b01cf840588fbfa8be0a071956b
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2021-07-07 23:22:44 +0300

    update

--- install1.sh
+++ install1.sh
@@ -8,4 +8,9 @@ sed -i 's/graphroot = "\/var\/lib\/containers\/storage"/graphroot = "\/_containe
 #sudo useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' 'RheujvDhfub72') podman
 #mkdir -p /home/podman/.config/systemd/user
 
+#Add GROUP and USER same as in container
+groupadd -r postgres --gid=9999
+useradd -r -M -g postgres --uid=9999 postgres
+
+#
 podman run --name pgpro -d -v /_data:/_data docker.io/docker/kostikpl/rhel8:pgpro-11.12.1_rhel-8.4
