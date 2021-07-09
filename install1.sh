730a967c3e7b9b00babd4e902e962a0c319453df
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2021-07-09 21:53:10 +0300

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -23,12 +23,17 @@ fi
 chown -R postgres:postgres /_data/pg_backup
 chmod -R 40700 /_data/pg_backup
 
+#Install HASP
+curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/hasp.sh
+bash hasp.sh
+
+HOSTNAME = `hostname`
 #Start PGPRO container and restore database
-podman run --name pgpro --ip 10.88.0.2 --hostname pgpro.local -dt -p 5432:5432 -v /_data:/_data docker.io/kostikpl/rhel8:pgpro-11.12.1_rhel-8.4
+podman run --name pgpro --ip 10.88.0.2 --hostname $HOSTNAME -dt -p 5432:5432 -v /_data:/_data docker.io/kostikpl/rhel8:pgpro-11.12.1_rhel-8.4
 podman generate systemd --new --name pgpro > /etc/systemd/system/pgpro.service
 systemctl enable --now pgpro
 
 #Start SRV1C container and restore database
-podman run --name srv1c --ip 10.88.0.3 --hostname rhel8.local --add-host=pgpro.local:10.88.0.2 -dt -p 1540-1541:1540-1541 -p 1560-1591:1560-1591 -v /_data:/_data docker.io/kostikpl/rhel8:srv1c-8.3.1_rhel-8.4
+podman run --name srv1c --ip 10.88.0.3 --hostname $HOSTNAME --add-host=pgpro:10.88.0.2 -dt -p 1540-1541:1540-1541 -p 1560-1591:1560-1591 -v /_data:/_data docker.io/kostikpl/rhel8:srv1c-8.3.1_rhel-8.4
 podman generate systemd --new --name srv1c > /etc/systemd/system/srv1c.service
 systemctl enable --now srv1c
