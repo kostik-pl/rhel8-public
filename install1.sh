d9bcf9d3b728b265dc1f729bcf5114194fbd64ef
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2021-07-09 18:35:27 +0300

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -11,6 +11,8 @@ sed -i 's/graphroot = "\/var\/lib\/containers\/storage"/graphroot = "\/_containe
 #Add GROUP and USER same as in container
 groupadd -r postgres --gid=9999
 useradd -r -M -g postgres --uid=9999 postgres
+groupadd -r srv1c --gid=9998
+useradd -r -M -g srv1c --uid=9998 srv1c
 
 #Check access rights
 chown -R root:root /_data
@@ -31,5 +33,7 @@ chmod -R 40700 /_data/pg_backup
 #chmod -R 40700 /_data/pg_data
 
 #Start PGPRO container and restore database
-podman run --name pgpro -d -v /_data:/_data docker.io/kostikpl/rhel8:pgpro-11.12.1_rhel-8.4
-podman exec -ti pgpro psql -f /_data/pg_backup/work.out  postgres
+podman run --name pgpro --ip 10.88.0.2 --hostname pgpro.local -dt -p 5432:5432 -v /_data:/_data docker.io/kostikpl/rhel8:pgpro-11.12.1_rhel-8.4
+
+#Start SRV1C container and restore database
+podman run --name srv1c --ip 10.88.0.3 --hostname rhel8.local --add-host=pgpro.local:10.88.0.2 -dt -p 1540-1541:1540-1541 -p 1560-1591:1560-1591 -v /_data:/_data docker.io/kostikpl/rhel8:srv1c-8.3.1_rhel-8.4
