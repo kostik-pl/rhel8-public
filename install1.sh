4d05594306ab57179ad3554b39b395e4e6b3e7c0
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2021-10-25 10:03:50 +0300

    Update

--- install1.sh
+++ install1.sh
@@ -65,7 +65,6 @@ podman run --name pgpro --ip 10.88.0.2 --hostname $HOSTNAME -dt -p 5432:5432 -v 
 podman generate systemd --new --name pgpro > /etc/systemd/system/pgpro.service
 systemctl enable --now pgpro
 podman exec -ti pgpro psql -c "ALTER USER postgres WITH PASSWORD RheujvDhfub12;"
-COMMAND='ALTER USER srv1c WITH PASSWORD ''$GitybwZ - ZxvtyM$'';'
 podman exec -ti pgpro psql -c "ALTER USER srv1c WITH PASSWORD '\'\$'GitybwZ ''-'' ZxvtyM'\$\';"
 
 #Install HASP
