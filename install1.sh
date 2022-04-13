fa54b3c00d3ab77a3169e2c2b5790d80fe9d9c95
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2022-04-13 18:36:40 +0300

    Commit

--- install1.sh
+++ install1.sh
@@ -61,11 +61,11 @@ firewall-cmd --reload
 HOSTNAME=`hostname`
 
 #Start PGPRO container and restore database
-podman run --name pgpro --ip 10.88.0.2 --hostname $HOSTNAME -dt -p 5432:5432 -v /_data:/_data docker.io/kostikpl/rhel8:pgpro-13.4.1_rhel-ubi-8.4
+podman run --name pgpro --ip 10.88.0.2 --hostname $HOSTNAME -dt -p 5432:5432 -v /_data:/_data docker.io/kostikpl/rhel8:pgpro-14.2.1_rhel-ubi-8.5
 podman generate systemd --new --name pgpro > /etc/systemd/system/pgpro.service
 systemctl enable --now pgpro
-podman exec -ti pgpro psql -c "ALTER USER postgres WITH PASSWORD RheujvDhfub12;"
-podman exec -ti pgpro psql -c "ALTER USER srv1c WITH PASSWORD '\'\$'GitybwZ ''-'' ZxvtyM'\$\';"
+podman exec -ti pgpro psql -c "ALTER USER postgres WITH PASSWORD "RheujvDhfub12";"
+podman exec -ti pgpro psql -c "ALTER USER srv1c WITH PASSWORD ""'\'\$'GitybwZ ''-'' ZxvtyM'\$\'";"
 
 #Install HASP
 curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/hasp.sh
