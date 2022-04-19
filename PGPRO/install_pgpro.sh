8fdcd3fecdbb70c877e2ac213164ba5b6e6a8cb8
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2022-04-19 21:22:25 +0300

    Update

--- PGPRO/install_pgpro.sh
+++ PGPRO/install_pgpro.sh
@@ -4,7 +4,8 @@ curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/PGPRO/pg
 curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/PGPRO/pgpro-entrypoint.sh
 podman build -t pgpro .
 podman run --name pgpro --ip 10.88.0.2 --hostname pgpro.local -dt -p 5432:5432 -v /_data:/_data localhost/pgpro
-podman push localhost\pgpro docker.io/kostikpl/rhel8:pgpro-14.2.1_rhel-ubi-8.5
-podman exec -ti pgpro psql -f /_data/pg_backup/work.out
+podman login -u=kostikpl -p=RheujvDhfub12 docker.io
+podman push pgpro docker.io/kostikpl/rhel8:pgpro-14.2.1_rhel-ubi-8.5
 
-podman run --name pgpro --ip 10.88.0.2 --hostname pgpro.local -dt -p 5432:5432 -v /_data:/_data docker.io/kostikpl/rhel8:rhel8:pgpro-14.2.1_rhel-ubi-8.5
+#podman run --name pgpro --ip 10.88.0.2 --hostname pgpro.local -dt -p 5432:5432 -v /_data:/_data docker.io/kostikpl/rhel8:rhel8:pgpro-14.2.1_rhel-ubi-8.5
+#podman exec -ti pgpro psql -f /_data/pg_backup/work.out
