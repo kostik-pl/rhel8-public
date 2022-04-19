41baaf0bcd404b1b2cfb92ea6b461004131d0dd2
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2022-04-19 20:49:26 +0300

    Update

--- PGPRO/install_pgpro.sh
+++ PGPRO/install_pgpro.sh
@@ -1,4 +1,7 @@
 curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/PGPRO/Dockerfile
+curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/PGPRO/RPM-GPG-KEY-POSTGRESPRO
+curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/PGPRO/pgpro-14.repo
+curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/PGPRO/pgpro-entrypoint.sh
 podman build -t pgpro .
 podman run --name pgpro --ip 10.88.0.2 --hostname pgpro.local -dt -p 5432:5432 -v /_data:/_data localhost/pgpro
 podman push localhost\pgpro docker.io/kostikpl/rhel8:pgpro-14.2.1_rhel-ubi-8.5
