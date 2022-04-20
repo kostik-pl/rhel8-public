d3b98ef77117c313b8456cc0b18ec7b0948cae27
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2022-04-20 14:52:44 +0300

    Update

--- 1c.sh
+++ 1c.sh
@@ -11,7 +11,7 @@ dnf install -y libpng12 #fontconfig libgsf freetype glib2 bzip2
 rm /etc/rc.d/init.d/srv1cv83
 curl -LJO https://www.dropbox.com/s/88q19pt9of2qok0/setup-full-8.3.20.1789-x86_64.run?dl=0
 chmod +x setup-full-8.3.20.1789-x86_64.run
-./setup-full-8.3.20.1789-x86_64.run --mode unattended --enable-components server,server_admin,ws,uk,ru
+./setup-full-8.3.20.1789-x86_64.run --mode unattended --enable-components server,server_admin,uk,ru
 curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/SRV1C_host/srv1cv83.service
 #cp srv1cv83.service /etc/systemd/system/
 curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/SRV1C_host/ras1cv83.service
