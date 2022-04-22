0d61fae3f137a14477dd9255d8b5e5e7a8a29b63
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2022-04-22 14:12:23 +0300

    Update

--- 1c.sh
+++ 1c.sh
@@ -9,7 +9,13 @@ dnf install -y libpng12 #fontconfig libgsf freetype glib2 bzip2
 
 # install 1C Enterprise server packages from work dir
 rm /etc/rc.d/init.d/srv1cv83
-curl -LJO https://www.dropbox.com/s/88q19pt9of2qok0/setup-full-8.3.20.1789-x86_64.run?dl=0
+#Download form GOOGLE
+filename="setup-full-8.3.20.1789-x86_64.run"
+fileid="1-93xicHCwe9IACSzwQh1mXwtw2o5TeEg"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
+#Donload from DROPBOX
+#curl -LJO https://www.dropbox.com/s/88q19pt9of2qok0/setup-full-8.3.20.1789-x86_64.run?dl=0
 chmod +x setup-full-8.3.20.1789-x86_64.run
 #ATTENTION! Batch installation will always install the 1c client and, if missing, the trimmed GNOME
 #./setup-full-8.3.20.1789-x86_64.run --mode unattended --enable-components server,server_admin,ws,uk,ru
