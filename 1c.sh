0ca1d184fa1922040b9b745c1a207f52cfaf9958
kostik-pl <kostik_pl@hotmail.com>
2022-05-05 09:31:32 +0300

    Update

--- 1c.sh
+++ 1c.sh
@@ -16,18 +16,18 @@ html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${f
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
 #Donload from DROPBOX
 #curl -LJO https://www.dropbox.com/s/88q19pt9of2qok0/setup-full-8.3.20.1789-x86_64.run?dl=0
-chmod +x setup-full-8.3.20.1789-x86_64.run
+chmod +x setup-full-8.3.21.1302-x86_64.run
 #ATTENTION! Batch installation will always install the 1c client and, if missing, the trimmed GNOME
 #./setup-full-8.3.20.1789-x86_64.run --mode unattended --enable-components server,server_admin,ws,uk,ru
 #We use pre-installed GNOME and manual installation
-./setup-full-8.3.20.1789-x86_64.run
+./setup-full-8.3.21.1302-x86_64.run
 
-ln -s /opt/1cv8/x86_64/8.3.20.1789/srv1cv83 /etc/init.d/srv1cv83
-ln -s /opt/1cv8/x86_64/8.3.20.1789/srv1cv83.conf /etc/sysconfig/srv1cv83
-sed -i 's/#SRV1CV8_DEBUG=/SRV1CV8_DEBUG=1/' /etc/sysconfig/srv1cv83
-sed -i 's/#SRV1CV8_DATA=/SRV1CV8_DATA=\/_data\/srv1c_inf_log/' /etc/sysconfig/srv1cv83
-chkconfig --add srv1cv83
-chkconfig srv1cv83 on
+#ln -s /opt/1cv8/x86_64/8.3.21.1302/srv1cv83 /etc/init.d/srv1cv83
+#ln -s /opt/1cv8/x86_64/8.3.21.1302/srv1cv83.conf /etc/sysconfig/srv1cv83
+sed -i 's/#SRV1CV8_DEBUG=/SRV1CV8_DEBUG=1/' /opt/1cv8/x86_64/8.3.21.1302/srv1cv83
+sed -i 's/#SRV1CV8_DATA=/SRV1CV8_DATA=\/_data\/srv1c_inf_log/' /opt/1cv8/x86_64/8.3.21.1302/srv1cv83
+#chkconfig --add srv1cv83
+#chkconfig srv1cv83 on
 
 curl -LJO https://raw.githubusercontent.com/kostik-pl/rhel8-public/main/SRV1C_host/ras1cv83.service
 cp ras1cv83.service /etc/systemd/system/
