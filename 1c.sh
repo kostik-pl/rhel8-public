465b02c47236d95366018466c332d17e42af291f
kostik-pl <kostik_pl@hotmail.com>
2022-05-05 21:56:22 +0300

    Update

--- 1c.sh
+++ 1c.sh
@@ -21,7 +21,7 @@ chmod +x setup-full-8.3.21.1302-x86_64.run
 #We use pre-installed GNOME and manual installation
 ./setup-full-8.3.21.1302-x86_64.run
 
-sed -i 's/Environment=SRV1CV8_DEBUG=/Environment=SRV1CV8_DEBUG=1/' /opt/1cv8/x86_64/8.3.21.1302/srv1cv8-8.3.21.1302@.service
+sed -i 's/Environment=SRV1CV8_DEBUG=/Environment=SRV1CV8_DEBUG=-debug/' /opt/1cv8/x86_64/8.3.21.1302/srv1cv8-8.3.21.1302@.service
 sed -i 's/Environment=SRV1CV8_DATA=\/home\/usr1cv8\/.1cv8\/1C\/1cv8/Environment=SRV1CV8_DATA=\/_data\/srv1c_inf_log/' /opt/1cv8/x86_64/8.3.21.1302/srv1cv8-8.3.21.1302@.service
 
 systemctl link /opt/1cv8/x86_64/8.3.21.1302/srv1cv8-8.3.21.1302@.service
